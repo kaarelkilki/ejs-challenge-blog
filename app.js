9f1ff2df71a51bb26aa74d360ffd013ff3c4a474
Kaarel <65207477+kaarelkilki@users.noreply.github.com>
2021-08-28 06:17:43 +0300

    Prettier formatting

--- app.js
+++ app.js
@@ -13,37 +13,39 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 let posts = [];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
   });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about", {
     aboutContent: aboutContent
   });
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", function(req, res) {
   res.render("contact", {
     contactContent: contactContent
   });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res) {
   res.render("compose", {
     contactContent: contactContent
   });
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
@@ -52,10 +54,10 @@ app.post("/compose", function(req, res){
   res.redirect("/");
 });
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function(req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
+  posts.forEach(function(post) {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
